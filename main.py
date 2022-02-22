c02e313942e888ab5dec3678e4ad1e72e627edac
Eldar101 <eldar101@gmail.com>
2022-02-22 18:27:21 +0200

    Deleting mistake

--- main.py
+++
@@ -1,8 +0,0 @@
-from tests.tests import *
-
-
-# This is just a main class to call all tests
-class Main:
-    test_magic_list_basic()
-    test_magic_list_type()
-    test_magic_list_continuity()
--- person.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-# This is a dataclass to define an age int, as described in the task
-@dataclass
-class Person(object):
-    age: int = 1
