647986f546fce2bd3db074625df8e825f0caefe0
Eldar101 <eldar101@gmail.com>
2022-07-19 11:44:48 +0300

    ocp-edge-auto script

---
+++ ocp-edge-auto.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+git config --global http.sslVerify false
+git clone https://gitlab.cee.redhat.com/eweiss/ocp-edge-auto.git
+cd ocp-edge-auto
+virtualenv .venv
+. .venv/bin/activate
+pip install -r requirements.txt
+cp ../clusterconfigs/auth/kubeconfig .
+cp ../install-config.yaml .
+export KUBECONFIG=kubeconfig
+export ASSISTED_INSTALLER=false
+export SNO=false
+export ENVIRONMENT="Virtual"
+echo '[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = https://gitlab.cee.redhat.com/eweiss/ocp-edge-auto.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+[remote "upstream"]
+        url = https://gitlab.cee.redhat.com/ocp-edge-qe/ocp-edge-auto.git
+        fetch = +refs/heads/*:refs/remotes/upstream/*
+[credential]
+        username = eweiss
+[credential]
+        password = "Blue30eldarweiss"'  >> /home/kni/ocp-edge-auto/.git/config
+git config pull.rebase false
+git config --global user.name "Eldar Weiss"
+git checkout master
+git fetch upstream
+git merge upstream/master
